45e2909c129d97912c2e38a3e89919fb3aaac0c2
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-04-05 11:31:08 -0400

    Time: 40 ms (93.83%), Space: 18.1 MB (9.06%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,16 +10,43 @@ class Node:
 
 
 class Solution:
-    def connect(self, node: "Optional[Node]") -> "Optional[Node]":
-        curr, nxt = node, node.left if node else None
+    def connect(self, root: "Optional[Node]") -> "Optional[Node]":
+        # curr, nxt = node, node.left if node else None
 
-        while curr and nxt:
-            curr.left.next = curr.right
-            if curr.next:
-                curr.right.next = curr.next.left
-            curr = curr.next
-            if not curr:
-                curr = nxt
-                nxt = curr.left
-        return node
+        # while curr and nxt:
+        #     curr.left.next = curr.right
+        #     if curr.next:
+        #         curr.right.next = curr.next.left
+        #     curr = curr.next
+        #     if not curr:
+        #         curr = nxt
+        #         nxt = curr.left
+        # return node
+
+
+        ########## 
+        if not root:
+            return root
+
+        # Start with the root node
+        queue = [root]
+
+        while queue:
+            size = len(queue)
+
+            # Iterate through nodes at the current level
+            for i in range(size):
+                node = queue.pop(0)
+
+                # Connect nodes at the same level
+                if i < size - 1:
+                    node.next = queue[0]
+
+                # Add child nodes to the queue
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return root
 
