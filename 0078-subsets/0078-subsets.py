45046e737a4527ef363048a3da36ff314c2b90b6
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-08-10 21:45:34 -0400

    Time: 31 ms (90.68%), Space: 16.8 MB (22.34%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subset, currsets = [], []
+
+        def helper(i, nums, subset, currsets):
+            # Correcting the condition to check if we've gone through all elements
+            if i >= len(nums):
+                subset.append(currsets.copy())  # Add a copy of the current subset
+                return
+            
+            # Decision to include nums[i]
+            currsets.append(nums[i])
+            helper(i + 1, nums, subset, currsets)
+            currsets.pop()  # Backtrack to remove the last element added
+
+            # Decision to not include nums[i]
+            helper(i + 1, nums, subset, currsets)
+        
+        helper(0, nums, subset, currsets)
+        return subset
