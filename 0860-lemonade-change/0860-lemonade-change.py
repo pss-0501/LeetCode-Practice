ab5fbad58f54dd43b878c4eaca3a6db7e1dce105
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-08-15 16:34:15 -0400

    Time: 613 ms (74.85%), Space: 21.1 MB (58.38%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,25 +1,49 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        change = []
-        for i in bills:
-            if i == 5:
-                change.append(5)
+        # change = []
+        # for i in bills:
+        #     if i == 5:
+        #         change.append(5)
+        #         continue
+        #     if i == 10 and change.count(5) >= 1:
+        #         change.remove(5)
+        #         change.append(10)
+        #         continue
+        #     if i == 20 and change.count(5) >= 1 and (change.count(5) >= 3 or change.count(10) >= 1):
+        #         if change.count(10) >= 1: 
+        #             change.remove(10)
+        #             change.remove(5)
+        #         else:
+        #             change.remove(5)
+        #             change.remove(5)
+        #             change.remove(5)
+        #         change.append(20)
+        #         continue
+        #     else:
+        #         return False
+
+        # return True
+
+        five = 0
+        ten = 0
+        for n in bills:
+            if n == 5:
+                five += 1
                 continue
-            if i == 10 and change.count(5) >= 1:
-                change.remove(5)
-                change.append(10)
+            if n == 10 and five >= 1:
+                five -= 1
+                ten += 1
                 continue
-            if i == 20 and change.count(5) >= 1 and (change.count(5) >= 3 or change.count(10) >= 1):
-                if change.count(10) >= 1: 
-                    change.remove(10)
-                    change.remove(5)
+            if n == 20:
+                if ten >= 1 and five >= 1:
+                    ten -= 1
+                    five -= 1
+                    continue
+                elif five >= 3:
+                    five -= 3
+                    continue
                 else:
-                    change.remove(5)
-                    change.remove(5)
-                    change.remove(5)
-                change.append(20)
-                continue
+                    return False
             else:
                 return False
-
         return True
