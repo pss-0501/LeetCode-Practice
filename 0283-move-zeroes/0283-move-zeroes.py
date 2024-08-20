00e4f8f4045ddc43124ccaee150ac0885cba34e2
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-08-20 01:08:48 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,14 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero = 0
-        temp = []
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                temp.append(nums[i])
+        # # Brute Force
+        # temp = []
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         temp.append(nums[i])
         
-        for k in range(len(temp)):
-            nums[k] = temp[k]
+        # for k in range(len(temp)):
+        #     nums[k] = temp[k]
         
-        for j in range(len(temp), len(nums)):
-            nums[j] = 0
+        # for j in range(len(temp), len(nums)):
+        #     nums[j] = 0
+
+        # Optimise
+        l = 0
+        r = l + 1
+        while r < len(nums):
+            if nums[r] != 0:
+                # Swap if `l` is not equal to `r` to avoid redundant swaps
+                if l != r:
+                    nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            r += 1
