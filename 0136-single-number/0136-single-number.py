f73df63657fcc555b7577a9a774e3a7f2e1a53b3
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-08-24 19:19:20 -0400

    Time: 114 ms (38.44%), Space: 18.9 MB (92.11%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,18 +1,18 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # Brute Force
-        for i in range(len(nums)):
-            found_duplicate = False
+        # for i in range(len(nums)):
+        #     found_duplicate = False
             
-            # Compare the current element with every other element in the array
-            for j in range(len(nums)):
-                if i != j and nums[i] == nums[j]:
-                    found_duplicate = True
-                    break
+        #     # Compare the current element with every other element in the array
+        #     for j in range(len(nums)):
+        #         if i != j and nums[i] == nums[j]:
+        #             found_duplicate = True
+        #             break
             
-            # If no duplicate is found, return the current element
-            if not found_duplicate:
-                return nums[i]
+        #     # If no duplicate is found, return the current element
+        #     if not found_duplicate:
+        #         return nums[i]
 
         # # Better Solution
         # mydict = {}
