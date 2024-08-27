df5d9908a96cc16689368bdae9e531f1c9a60678
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-08-27 02:08:03 -0400

    Time: 53 ms (87.4%), Space: 17.8 MB (29.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,23 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        my_dict = {}
-        for i,num in enumerate(nums):
-            sub = target - num
-            if sub in my_dict:
-                return [my_dict[sub], i]    # return value of dict[key] & i
-            my_dict[num] = i
-        return[]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute Force
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
 
+        # return []
         
+        # Optimal
+        add_dict = {}
+        n = len(nums)
+        for i in range(n):
+            rem = target - nums[i]
+            if rem in add_dict:
+                return [add_dict[rem],i]
+            
+            add_dict[nums[i]] = i
+
+        return []
+
