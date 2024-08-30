a0c5e735539e364ca0b57594d0da7b0e4833ea80
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-08-29 23:18:49 -0400

    Time: 31 ms (90.69%), Space: 16.4 MB (66.61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,27 +4,42 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # Better sol
-        c0 = 0
-        c1 = 0
-        c2 = 0
+        # c0 = 0
+        # c1 = 0
+        # c2 = 0
 
-        for i in nums:
-            if i == 0:
-                c0 += 1
-            elif i == 1:
-                c1 += 1
-            else:
-                c2 += 1
+        # for i in nums:
+        #     if i == 0:
+        #         c0 += 1
+        #     elif i == 1:
+        #         c1 += 1
+        #     else:
+        #         c2 += 1
 
-        for i in range(c0):
-            nums[i] = 0
+        # for i in range(c0):
+        #     nums[i] = 0
 
-        for j in range(c0,c0 + c1):
-            nums[j] = 1
+        # for j in range(c0,c0 + c1):
+        #     nums[j] = 1
 
-        for k in range(c0 + c1, c0 + c1 + c2):
-            nums[k] = 2
+        # for k in range(c0 + c1, c0 + c1 + c2):
+        #     nums[k] = 2
 
-        return nums
+        # return nums
             
         # DNF Algo needs to be used
+        n = len(nums)
+        low = 0
+        mid = 0
+        high = n-1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] ==1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
