e04535230d2ce48304a1364ef762cfedd2c7456f
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-08-29 22:22:15 -0400

    Time: 39 ms (45.95%), Space: 16.4 MB (66.61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,5 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
-        
+        # Better sol
+        c0 = 0
+        c1 = 0
+        c2 = 0
+
+        for i in nums:
+            if i == 0:
+                c0 += 1
+            elif i == 1:
+                c1 += 1
+            else:
+                c2 += 1
+
+        for i in range(c0):
+            nums[i] = 0
+
+        for j in range(c0,c0 + c1):
+            nums[j] = 1
+
+        for k in range(c0 + c1, c0 + c1 + c2):
+            nums[k] = 2
+
+        return nums
+            
+        # DNF Algo needs to be used
