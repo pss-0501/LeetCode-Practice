84fbb201c1142b7a87af00529e5d87e9f2fce1be
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-09-01 18:03:06 -0400

    Time: 1002 ms (56.7%), Space: 46.9 MB (78.64%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,20 +1,33 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos = []
-        neg = []
+        # Brute Force
+        # pos = []
+        # neg = []
+        # for i in range(len(nums)):
+        #     if nums[i] > 0:
+        #         pos.append(nums[i])
+        #     elif nums[i] < 0:
+        #         neg.append(nums[i])
+
+        # n = len(nums) // 2
+        # nums = []
+
+        # for i in range(n):
+        #     nums.append(pos[i])
+        #     nums.append(neg[i])
+
+        # return nums
+
+        # Optimise
+        pos = 0
+        neg = 1
+        ans = [0] * len(nums)
         for i in range(len(nums)):
             if nums[i] > 0:
-                pos.append(nums[i])
-            elif nums[i] < 0:
-                neg.append(nums[i])
+                ans[pos] = nums[i]
+                pos += 2
+            else:
+                ans[neg] = nums[i]
+                neg += 2
 
-        n = len(nums) // 2
-        nums = []
-
-        for i in range(n):
-            # if i % 2 ==0:
-            nums.append(pos[i])
-            # else:
-            nums.append(neg[i])
-
-        return nums
+        return ans
