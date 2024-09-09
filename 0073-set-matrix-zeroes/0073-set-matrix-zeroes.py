362ff43f5005c931d93edcdb0d24f7bf52ed938c
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-09-09 01:55:46 -0400

    Time: 108 ms (25.61%), Space: 17.5 MB (57.13%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,26 +3,49 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        # Better Solution        
         n = len(matrix)
         m = len(matrix[0])
 
-        def matRow(i):
-            for j in range(m):
-                if matrix[i][j] != 0:
-                    matrix[i][j] = float('inf')
-
-        def matCol(j):
-            for i in range(n):
-                if matrix[i][j] != 0:
-                    matrix[i][j] = float('inf')
+        row = [0] * n
+        col = [0] * m
 
         for i in range(n):
             for j in range(m):
                 if matrix[i][j] == 0:
-                    matRow(i)
-                    matCol(j)
+                    row[i] = 1
+                    col[j] = 1
 
         for i in range(n):
             for j in range(m):
-                if matrix[i][j] == float('inf'):
+                if row[i] == 1 or col[j] == 1:
                     matrix[i][j] = 0
+
+
+
+
+
+        # Brute Force
+        # n = len(matrix)
+        # m = len(matrix[0])
+
+        # def matRow(i):
+        #     for j in range(m):
+        #         if matrix[i][j] != 0:
+        #             matrix[i][j] = float('inf')
+
+        # def matCol(j):
+        #     for i in range(n):
+        #         if matrix[i][j] != 0:
+        #             matrix[i][j] = float('inf')
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         if matrix[i][j] == 0:
+        #             matRow(i)
+        #             matCol(j)
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         if matrix[i][j] == float('inf'):
+        #             matrix[i][j] = 0
