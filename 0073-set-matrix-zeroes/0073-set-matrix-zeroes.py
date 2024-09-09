7a7eb04fcd8151a45e8e8be9793ebf593b7adfd3
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-09-09 01:56:35 -0400

    Time: 102 ms (61.19%), Space: 17.4 MB (57.13%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,26 +3,57 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # Better Solution        
+        # Optimise
+        col0 = 1
         n = len(matrix)
         m = len(matrix[0])
-
-        row = [0] * n
-        col = [0] * m
-
         for i in range(n):
             for j in range(m):
                 if matrix[i][j] == 0:
-                    row[i] = 1
-                    col[j] = 1
+                    matrix[i][0] = 0
 
-        for i in range(n):
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+
+        for i in range(1, n):
+            for j in range(1, m):
+                if matrix[i][j] != 0:
+                    # check for col & row:
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
             for j in range(m):
-                if row[i] == 1 or col[j] == 1:
-                    matrix[i][j] = 0
+                matrix[0][j] = 0
+        if col0 == 0:
+            for i in range(n):
+                matrix[i][0] = 0
 
 
 
+
+
+
+
+        # Better Solution        
+        # n = len(matrix)
+        # m = len(matrix[0])
+
+        # row = [0] * n
+        # col = [0] * m
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         if matrix[i][j] == 0:
+        #             row[i] = 1
+        #             col[j] = 1
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         if row[i] == 1 or col[j] == 1:
+        #             matrix[i][j] = 0
 
 
         # Brute Force
