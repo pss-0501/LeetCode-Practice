fc5da6b34f59faf0c81fb3b7151f7e3e586e6079
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-09-19 01:47:52 -0400

    Time: 130 ms (92.49%), Space: 26.7 MB (10.75%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,17 +1,40 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
+        # Brute Force
+
+        # n = len(nums)
+        # if n == 1:
+        #     return nums[0]
+        # for i in range(n):
+        #     if i == 0:
+        #         if nums[i] != nums[i + 1]:
+        #             return nums[i]
+        #     elif i == n - 1:
+        #         if nums[i] != nums[i - 1]:
+        #             return nums[i]
+        #     else:
+        #         if nums[i] != nums[i - 1] and nums[i] != nums[i + 1]:
+        #             return nums[i]
+
+        # Optimise
         n = len(nums)
         if n == 1:
             return nums[0]
-        for i in range(n):
-            if i == 0:
-                if nums[i] != nums[i + 1]:
-                    return nums[i]
-            elif i == n - 1:
-                if nums[i] != nums[i - 1]:
-                    return nums[i]
+        if nums[0] != nums[1]:
+            return nums[0]
+        if nums[n - 1] != nums[n - 2]:
+            return nums[n-1]
+        low = 1
+        high = n - 2
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
+                return nums[mid]
+            # left side even,odd
+            if (mid % 2 == 0 and nums[mid] == nums[mid + 1]) or (mid % 2 == 1 and nums[mid] == nums[mid - 1]):
+                low = mid + 1
             else:
-                if nums[i] != nums[i - 1] and nums[i] != nums[i + 1]:
-                    return nums[i]
+                high = mid - 1
 
+        return -1
             
