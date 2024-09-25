b3e3e43a0110d7bf0bf60227f29d5037a5a66e46
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-09-25 01:54:10 -0400

    Time: 136 ms (74.68%), Space: 23 MB (43.61%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,25 +1,42 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         # BF O n**2
-        # Better Solution
-        def BS(arr, k):
-            low = 0
-            high = len(arr) - 1
-            while low <= high:
-                mid = (low + high) // 2
-                if arr[mid] == k:
-                    return mid
-                elif arr[mid] < k:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-            return -1
+        # # Better Solution
+        # def BS(arr, k):
+        #     low = 0
+        #     high = len(arr) - 1
+        #     while low <= high:
+        #         mid = (low + high) // 2
+        #         if arr[mid] == k:
+        #             return mid
+        #         elif arr[mid] < k:
+        #             low = mid + 1
+        #         else:
+        #             high = mid - 1
+        #     return -1
 
-        row = len(matrix)
-        col = len(matrix[0])
+        # row = len(matrix)
+        # col = len(matrix[0])
 
-        for i in range(row):
-            index = BS(matrix[i], target)
-            if index != -1:
+        # for i in range(row):
+        #     index = BS(matrix[i], target)
+        #     if index != -1:
+        #         return True
+        # return False
+
+        # Optimise
+        if not matrix or not matrix[0]:
+            return False
+        
+        row = 0
+        col = len(matrix[0]) - 1
+        
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] == target:
                 return True
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
+        
         return False
