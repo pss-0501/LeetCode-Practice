a8ee86a8ae2d468cdad68a00b1e79e33d1f48059
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-10-01 11:21:13 -0400

    Time: 304 ms (33.6%), Space: 36.9 MB (28.68%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,49 +4,19 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # def isPalindrome(self, head: Optional[ListNode]) -> bool:
-    #     # array solution
-
-    #     myarr = []
-    #     while head:
-    #         myarr.append(head.val)
-    #         head = head.next
-
-    #     left, right = 0, len(myarr) - 1
-    #     while left <= right:
-    #         if myarr[left] != myarr[right]:
-    #             return False
-    #         left += 1
-    #         right -= 1
-    #     return True
-
-    # solution 2
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        fast = head
-        slow = head
+        curr = head
+        stack = []
+        while curr:
+            stack.append(curr.val)
+            curr = curr.next
 
-        # to get middle term
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-
-        # reverse 2nd half:
-        # prev will be at the last node
-        prev = None
-        while slow:
-            tmp = slow.next
-            slow.next = prev
-            prev = slow
-            slow = tmp
-
-        # check pali
-        left, right = head, prev
-        while right:
-            if left.val != right.val:
+        curr = head
+        while curr:
+            if curr.val != stack.pop():
                 return False
-            left = left.next
-            right = right.next
+            curr = curr.next
+
         return True
 
-
-
+            
