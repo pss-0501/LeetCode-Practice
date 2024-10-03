066f71b15c63afe7b5472cc9708f3d04af0e5ccc
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-10-03 03:24:01 -0400

    Time: 38 ms (46.26%), Space: 16.5 MB (71.3%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,32 +5,52 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # BF
+        # if head.next is None:
+        #     return None
+
+        # count = 0
+        # curr = head
+        # while curr:
+        #     count += 1
+        #     curr = curr.next
+
+        # m = count - n
+        # if m == 0:
+        #     temp = curr
+        #     curr = curr.next
+        #     temp.next = None
+        #     return curr
+      
+        # curr = head
+        # while curr and curr.next:
+        #     m -= 1
+        #     if m == 0:
+        #         temp = curr.next
+        #         curr.next = curr.next.next
+        #         temp.next = None                
+        #     curr = curr.next
+
+        # return head
+
+        # Optimise
+
         if head.next is None:
             return None
 
-        count = 0
-        curr = head
-        while curr:
-            count += 1
-            curr = curr.next
+        slow = head
+        fast = head
+        for _ in range(n):
+            fast = fast.next
 
-        m = (count - n)
+        if fast == None:
+            temp = head
+            head = head.next
+            return head
 
-        curr = head
-        if m == 0 and curr == head:
-            temp = curr
-            curr = curr.next
-            temp.next = None
-            return curr
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next
 
-        while curr and curr.next:
-            m -= 1
-            if m == 0:
-                temp = curr.next
-                curr.next = curr.next.next
-                temp.next = None
-                
-            curr = curr.next
-              
-
+        slow.next = slow.next.next
         return head
