c72f5229fb0c4f5de4526f12d24d0794ad5d7b92
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-10-09 19:20:26 -0400

    Time: 77 ms (75.75%), Space: 27.2 MB (31.48%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,17 +6,57 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        myDict = {}
-        curr1 = headA
+        # BF
+
+        # myDict = {}
+        # curr1 = headA
         
+        # while curr1:
+        #     myDict[curr1] = 1
+        #     curr1 = curr1.next
+
+        # curr2 = headB
+        # while curr2:
+        #     if curr2 in myDict:
+        #         return curr2
+        #     curr2 = curr2.next
+
+        # return None
+
+        # Optimise
+
+        curr1 = headA
+        lenA = 0
         while curr1:
-            myDict[curr1] = 1
+            lenA += 1
             curr1 = curr1.next
 
         curr2 = headB
+        lenB = 0
         while curr2:
-            if curr2 in myDict:
-                return curr2
+            lenB += 1
             curr2 = curr2.next
+
+        if lenA < lenB:
+            tA = headA
+            tB = headB
+            d = lenB - lenA
+            while d > 0:
+                tB = tB.next
+                d -= 1
+        else:
+            tB = headB
+            tA = headA
+            d = lenA - lenB
+            while d > 0:
+                tA = tA.next
+                d -= 1
+
+        while tA and tB:
+            if tA == tB:
+                return tA
+            
+            tA = tA.next
+            tB = tB.next
 
         return None
