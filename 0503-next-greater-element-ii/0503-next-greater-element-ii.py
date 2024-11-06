8be4857754880db20e092359ae2dc52ad4c89082
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-11-05 19:05:18 -0500

    Time: 2034 ms (6.48%), Space: 18 MB (99.69%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,14 +1,16 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        # Brute Force
         nge = [-1] * len(nums)
 
-        arr = nums + nums
+        # arr = nums + nums
 
         n = len(nums)
         for i in range(n):
             for j in range(i + 1, i + n):
-                if arr[j] > nums[i]:
-                    nge[i] = arr[j]
+                idx = j % n
+                if nums[idx] > nums[i]:
+                    nge[i] = nums[idx]
                     break
 
         return nge
