749ebe65a18263b3ca91da833102440dc517d412
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2024-11-13 20:54:10 -0500

    Time: 15 ms (84.28%), Space: 16.9 MB (42.67%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,13 +1,49 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        sum1 = 0
-        n = len(nums)
-        for i in range(n):
-            small = nums[i]
-            large = nums[i]
-            for j in range(i + 1, n):
-                large = max(nums[j], large)
-                small = min(nums[j], small)
-                sum1 = sum1 + (large - small)
+        # Brute Force
+        # sum1 = 0
+        # n = len(nums)
+        # for i in range(n):
+        #     small = nums[i]
+        #     large = nums[i]
+        #     for j in range(i + 1, n):
+        #         large = max(nums[j], large)
+        #         small = min(nums[j], small)
+        #         sum1 = sum1 + (large - small)
 
-        return sum1
+        # return sum1
+
+        # Optimse
+        def max_sum(nums):
+            stack = []
+            res = 0
+            n = len(nums)
+
+            for i in range(n + 1):
+                while stack and (i == n or nums[stack[-1]] < nums[i]):
+                    j = stack.pop()
+                    left = j - (stack[-1] if stack else -1)
+                    right = i - j
+                    res += nums[j] * left * right
+                stack.append(i)
+            return res
+
+        def min_sum(nums):
+            stack = []
+            res = 0
+            n = len(nums)
+
+            for i in range(n + 1):
+                while stack and (i == n or nums[stack[-1]] > nums[i]):
+                    j = stack.pop()
+                    left = j - (stack[-1] if stack else -1)
+                    right = i - j
+                    res += nums[j] * left * right
+                stack.append(i)
+            
+            return res
+
+        max1 = max_sum(nums)
+        min1 = min_sum(nums)
+        return max1 - min1
+        
