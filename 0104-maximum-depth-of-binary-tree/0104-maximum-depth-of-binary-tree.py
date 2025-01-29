b4994729b5c0b68cf52970f1bc98ce2254aafda3
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2025-01-28 21:32:27 -0500

    Time: 0 ms (100%), Space: 19.2 MB (5.78%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,26 +6,41 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # BFS
+        # if not root:
+        #     return 0
+
+        # queue = deque()
+        # queue.append(root)
+        # # res = []
+        # count = 0
+
+        # while queue:
+        #     level = []
+        #     for _ in range(len(queue)):
+        #         node = queue.popleft()
+        #         level.append(node.val)
+                
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+
+        #     count += 1   
+        
+        # return count
+
+        # DFS
+        r = 0
+        l = 0
         if not root:
             return 0
 
-        queue = deque()
-        queue.append(root)
-        # res = []
-        count = 0
+        if root.left:
+            l = self.maxDepth(root.left)
 
-        while queue:
-            level = []
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                level.append(node.val)
-                
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+        if root.right:
+            r = self.maxDepth(root.right)
 
-            count += 1   
-        
-        return count
+        return 1 + max(l, r)
 
