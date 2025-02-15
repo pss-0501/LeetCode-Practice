fce866adcacc20c264d0a3b80cd9101aae1ed602
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2025-02-15 17:24:02 -0500

    Time: 212 ms (93.46%), Space: 17.7 MB (84.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,12 +1,41 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         # Brute Force
-        n = len(s)
-        longest = ""
-        for i in range(n):
-            for j in range(i, n):
-                sub = s[i:j + 1]
-                if sub == sub[::-1] and len(sub) > len(longest):
-                    longest = sub
+        # n = len(s)
+        # longest = ""
+        # for i in range(n):
+        #     for j in range(i, n):
+        #         sub = s[i:j + 1]
+        #         if sub == sub[::-1] and len(sub) > len(longest):
+        #             longest = sub
 
-        return longest
+        # return longest
+
+        # Optimise
+        if len(s) <= 1:
+            return s
+
+        def expand(left, right):
+            n = len(s)
+
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+
+            return s[left + 1: right]
+
+        n = len(s)
+        long_str = s[0]
+
+        for i in range(n - 1):
+            odd_center = expand(i, i)
+            even_center = expand(i, i + 1)
+
+            if len(odd_center) > len(long_str):
+                long_str = odd_center
+
+            if len(even_center) > len(long_str):
+                long_str = even_center
+
+
+        return long_str
