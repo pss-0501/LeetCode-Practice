17cf0a92231020fb136faf327287d1055f308f15
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2025-03-03 18:57:24 -0500

    Time: 0 ms (100%), Space: 18.6 MB (98.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,25 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # # Brute Force
-        # temp = []
-        # for i in range(len(nums)):
-        #     if nums[i] != 0:
-        #         temp.append(nums[i])
-        
-        # for k in range(len(temp)):
-        #     nums[k] = temp[k]
-        
-        # for j in range(len(temp), len(nums)):
-        #     nums[j] = 0
+        tmp = []
+        for i in nums:
+            if i != 0:
+                tmp.append(i)
 
-        # Optimise
-        l = 0
-        r = l + 1
-        while r < len(nums):
-            if nums[r] != 0:
-                # Swap if `l` is not equal to `r` to avoid redundant swaps
-                if l != r:
-                    nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-            r += 1
+        for i in range(len(tmp)):
+            nums[i] = tmp[i]
+
+        for i in range(len(tmp), len(nums)):
+            nums[i] = 0 
+
+        # n = len(nums)
+        # l = 0
+        # r = l
+        # while r < n:      
+        #     if nums[r] != 0:
+        #         if l != r:
+        #             nums[l], nums[r] = nums[r], nums[l]
+        #         l += 1
+        #     r += 1
