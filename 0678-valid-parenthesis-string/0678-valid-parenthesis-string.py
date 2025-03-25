895bd4d204b4d1875753ce050a2e1e8a94c231c8
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2025-03-25 15:14:13 -0400

    Time: 0 ms (100%), Space: 17.9 MB (41.03%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,20 +1,22 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        leftmin = 0
-        leftmax = 0
+        min_open = 0  # Minimum open brackets needed
+        max_open = 0  # Maximum open brackets possible
 
-        for i in s:
-            if i == '(':
-                leftmin += 1
-                leftmax += 1
-            elif i == ')':
-                leftmin -= 1
-                leftmax -= 1
-            else:
-                leftmin -= 1
-                leftmax += 1
-            if leftmax < 0:
+        for char in s:
+            if char == '(':
+                min_open += 1  # We need one more open bracket
+                max_open += 1  # Max count also increases
+            elif char == ')':
+                min_open = max(0, min_open - 1)  # Reduce min open, but not below 0
+                max_open -= 1  # Reduce max open count
+            else:  # '*' can act as '(', ')' or ''
+                min_open = max(0, min_open - 1)  # Reduce min count (acting as ')')
+                max_open += 1  # Increase max count (acting as '(')
+
+            # If max_open drops below 0, too many ')' were found
+            if max_open < 0:
                 return False
-            if leftmin < 0:
-                leftmin = 0
-        return leftmin == 0
+
+        # If min_open is 0, we can successfully balance the string
+        return min_open == 0
