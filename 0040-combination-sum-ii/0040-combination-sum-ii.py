4f4647ab0b2dcd4ab3200e5dbc600fe46e369cba
Prathamesh Santosh Sawant <127247670+pss-0501@users.noreply.github.com>
2025-04-01 17:59:16 -0400

    Time: 31 ms (27.41%), Space: 17.7 MB (84.08%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,28 +1,29 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
         candidates.sort()
-        combs = []
 
-        def helper(i, curComb):
-            if sum(curComb) == target:
-                combs.append(curComb.copy())
+        def dfs(i, curr, total):
+            if total == target:
+                res.append(curr.copy())
                 return
 
-            if sum(curComb) > target:
-                return
-            
-            if i >= len(candidates):
+            if i >= len(candidates) or total > target:
                 return
 
-            # Include candidates[i]
-            curComb.append(candidates[i])
-            helper(i + 1, curComb)  # Include the same element again
+            # include
+            curr.append(candidates[i])
+            dfs(i + 1, curr, total + candidates[i])
 
-            # Backtrack
-            curComb.pop()
+            # dont include
+            curr.pop()
             while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
                 i += 1
-            helper(i + 1, curComb)  # Move to the next element
-        
-        helper(0, [])
-        return combs
+            dfs(i + 1, curr, total)
+
+        dfs(0, [], 0)
+        return res
+
+
+
+        return
